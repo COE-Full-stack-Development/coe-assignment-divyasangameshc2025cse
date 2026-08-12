badcdd4326baed9acb11205f7bba656692cfc2ca
divyasangamesh2025cse <divyasangamesh.c2025cse@sece.ac.in>
2026-08-12 19:19:31 +0530

    completeted

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-(() => {
-  
-// your code goe here
+const skillInput = document.getElementById("skill-input");
+const addSkillBtn = document.getElementById("add-skill-btn");
+const skillsList = document.getElementById("skills-list");
 
-  });
-})();
+addSkillBtn.addEventListener("click", function () {
+  const skill = skillInput.value.trim();
+
+  if (skill !== "") {
+    const li = document.createElement("li");
+    li.textContent = skill;
+
+    skillsList.appendChild(li);
+
+    skillInput.value = "";
+  }
+});
